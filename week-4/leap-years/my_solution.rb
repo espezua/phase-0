a623b7c20e533512a7bfec4d1dd5d1cf9147291c
espezua <jennaespezua@gmail.com>
2015-11-12 14:39:48 -0500

    Add leap year and triangle solutions

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -4,3 +4,14 @@
 
 
 # Your Solution Below
+def leap_year?(year)
+  if  (year%4 == 0 ) && !(year%100 == 0)
+    return true
+  elsif (year%400 == 0)
+    return true
+  elsif (year%4 == 0 ) && (year%100 == 0) && !(year%400 == 0)
+    return false
+  elsif !(year%4 == 0 )
+    return false
+  end
+end
