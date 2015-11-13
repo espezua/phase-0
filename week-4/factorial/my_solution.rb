9cfe057f2d5aa6cb3850a372866448797869d8e4
espezua <jennaespezua@gmail.com>
2015-11-13 17:28:17 -0500

    Modify format

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -22,19 +22,15 @@ def factorial(number)
   end
 end
 =end
+
 =begin
 def factorial(n)
-  if n < 0
-    return nil
-  end
-
   result = 1
   while n > 0
     result = result * n
 
     n -= 1
   end
-
   return result
 end
 =end
@@ -43,6 +39,5 @@ end
 
 def factorial(n)
   if n == 0; return 1; end
-  (1..n).reduce(:*)
+  return (1..n).reduce(:*)
 end
-
