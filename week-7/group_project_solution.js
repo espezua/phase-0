fd4f23136e23b12d9c8388ce63522c64e59039bc
espezua <jennaespezua@gmail.com>
2015-12-03 11:38:08 -0500

    Add group project pseudocode

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,21 @@
+// Release 2
+// Pseudocode
+
+// Sum Function:
+// define a sum function that takes in an array of integers.
+// create a result variable equal to 0.
+// iterate through the array and add each array value to the result variable.
+// return the result variable.
+
+// Mean Function: (you can copy paste the sum function, then just change what it returns)
+// define a mean function that takes in an array of integers.
+// create a result variable equal to 0.
+// iterate through the array and add each array value to the result variable.
+// return the result variable divided by the array's length.
+
+// Median Function:
+// define a median function that takes in an array of integers.
+// create an if/else statement to check if the arrays length is even.
+// inside that if statement, set the result equal to: the two middle values of the array, that have been added together and divided by 2.
+// inside the else statement, set the result equal to: the middle value of the array.
+// return the result variable.
