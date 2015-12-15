65d5eded5bfb7770ba293f3cccfe19130ad4ff62
espezua <jennaespezua@gmail.com>
2015-12-15 15:45:38 -0500

    Add javascript review file

---
+++ week-9/review.js
@@ -0,0 +1,15 @@
+
+
+//Reflection
+/*
+What concepts did you solidify in working on this challenge?
+(reviewing the passing of information, objects, constructors, etc.)
+
+
+What was the most difficult part of this challenge?
+
+
+Did an array or object make more sense to use and why?
+
+
+*/
